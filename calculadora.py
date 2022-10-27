d30a75bc71fd3a6b5fc4e63bea5668248f1900fd
Daniela Hidalgo <hdanielad2001@gmail.com>
2022-10-27 15:56:58 -0500

    Se creo la calculadora

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+class Command:
+    '''
+	Clase base para implemantar los commandos que ejecutan
+	las operaciones matematicas de la calculadora.
+	'''
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def execute(self):
+        '''
+        por favor sobreescribir este metodo en cada commando.
+        '''
+        raise Exception('Overwrite this one pls')
